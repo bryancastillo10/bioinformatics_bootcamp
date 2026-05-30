214af01ad77c06297050959f3f290bf06f3dca46
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-05-30 19:15:20 +0800

    ✨ python solution

--- rosalind/stronghold/cons/python/main.py
+++ rosalind/stronghold/cons/python/main.py
@@ -20,15 +20,55 @@ def parse_fasta(data: str) -> list[str]:
 def build_mat(sequences: list[str]) -> dict[str, list[int]]:
     seq_length = len(sequences[0])
 
-	profile = {
-		"A": [0] * seq_length,
-		"C": [0] * seq_length,
-		"G": [0] * seq_length,
-		"T": [0] * seq_length
-	}
+    profile = {
+        "A": [0] * seq_length,
+        "C": [0] * seq_length,
+        "G": [0] * seq_length,
+        "T": [0] * seq_length,
+    }
 
-	for seq in sequences:
-		for idx, nucleotide in enumerate(seq):
-			profile[nucleotide][index] += 1
+    for seq in sequences:
+        for idx, nuc in enumerate(seq):
+            profile[nuc][idx] += 1
 
-	return profile
+    return profile
+
+
+def build_cons_string(profile: dict[str, list[int]]) -> str:
+    cons = ""
+    nucleotides = ["A", "C", "G", "T"]
+    seq_length = len(profile["A"])
+
+    for idx in range(seq_length):
+        most_common_nuc = "A"
+        highest_count = profile["A"][idx]
+
+        for nuc in nucleotides:
+            count = profile[nuc][idx]
+
+            if count > highest_count:
+                highest_count = count
+                most_common_nuc = nuc
+
+        cons += most_common_nuc
+
+    return cons
+
+
+def main():
+    with open("../input.txt", "r") as file:
+        data = file.read()
+
+    seq = parse_fasta(data)
+    profile = build_mat(seq)
+    cons = build_cons_string(profile)
+
+    print(cons)
+
+    for nuc in ["A", "G", "C", "T"]:
+        counts = " ".join(map(str, profile[nuc]))
+        print(f"{nuc}:{counts}")
+
+
+if __name__ == "__main__":
+    main()
