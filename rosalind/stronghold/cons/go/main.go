6fdaf828a927b1c398421b4a8fdf507c8390f09a
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-06-01 15:19:51 +0800

    🌐golang solution

--- rosalind/stronghold/cons/go/main.go
+++ rosalind/stronghold/cons/go/main.go
@@ -1,28 +1,39 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func parseFasta (data string) map[string]string {
-	records := make(map[string]string)
-	currID := ""
+func parseFasta (data string) []string {
+	sequences := []string{}
+	currSeq := ""
 
 	lines := strings.Split(strings.TrimSpace(data),"\n")
 
-	for _,line := range lines {
+	for _, line := range lines {
 		line = strings.TrimSpace(line)
 
 		if strings.HasPrefix(line,">") {
-			currID = strings.TrimPrefix(line,">")
-			records[currID] = ""
+			if currSeq != "" {
+				sequences = append(sequences, currSeq)
+				currSeq = ""
+			}
 		} else {
-			records[currID] += line
+			currSeq += line
 		}
 	}
 
-	return  records
+	if currSeq != "" {
+		sequences = append(sequences, currSeq)
+	}
+
+	return sequences
 }
 
-func buildProfileMatrix(sequences []string) map[string][] int {
+func buildProfileMatrix(sequences []string) map[string][]int {
 	seqLength := len(sequences[0])
 
 	profile := map[string][]int {
@@ -40,4 +51,56 @@ func buildProfileMatrix(sequences []string) map[string][] int {
 	}
 
 	return  profile
+}
+
+func buildConsensusString(profile map[string][]int) string {
+	var consensus strings.Builder
+
+	nucleotides := []string{"A","C","G","T",}
+	seqLength := len(profile["A"])
+
+	for idx := 0; idx < seqLength; idx++ {
+		mostCommonNuc := "A"
+		highestCount := profile["A"][idx]
+
+		for _, nuc := range nucleotides {
+			count := profile[nuc][idx]
+
+			if count > highestCount {
+				highestCount = count
+				mostCommonNuc = nuc
+			}
+		}
+
+		consensus.WriteString(mostCommonNuc)
+	}
+
+	return consensus.String()
+}
+
+func formatCounts(counts []int) string {
+	res := []string{}
+
+	for _, count := range counts {
+		res = append(res, strconv.Itoa(count))
+	}
+
+	return strings.Join(res," ")
+}
+
+func main() {
+	data, err := os.ReadFile("../input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	seq := parseFasta(string(data))
+	profile := buildProfileMatrix(seq)
+	consensus := buildConsensusString(profile)
+
+	fmt.Println(consensus)
+
+	for _, nuc := range []string{"A","C","G","T",}{
+		fmt.Printf("%s: %s\n",nuc, formatCounts(profile[nuc]))
+	}
 }
